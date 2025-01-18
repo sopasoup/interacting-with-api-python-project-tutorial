fbc98c1c12eb9f061b26abbb12738165a7941e44
Aldo <157986984+sopasoup@users.noreply.github.com>
2025-01-18 01:18:06 +0000

    en progreso, con errores

--- src/app.py
+++ src/app.py
@@ -1,7 +1,30 @@
 import os
 import pandas as pd
-import seaborn as sns
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# load the .env file variables
+# Cargar las variables del archivo .env
 load_dotenv()
+
+# Obtener CLIENT_ID y CLIENT_SECRET del entorno
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id,
+                                                           client_secret=client_secret))
+
+artist_id = "4gzpq5DPGxSnKTe4SA8HAU"
+
+response = sp.artist_top_tracks(artist_id)
+if response:
+    tracks = response["tracks"]
+    tracks = [{k: (v / (1000 * 60)) % 60 if k == "duration_ms" else v for k, v in track.items()
+               if k in ["name", "popularity", "duration_ms"]} for track in tracks]
+    
+tracks_df = pd.DataFrame.from_records(tracks)
+tracks_df.sort_values(["popularity"], inplace=True)
+
+print(tracks_df.tail())
