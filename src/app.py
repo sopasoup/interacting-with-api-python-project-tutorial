070938703cd42468850a56e8e48d787536c143e8
Aldo <157986984+sopasoup@users.noreply.github.com>
2025-01-21 00:33:54 +0000

    por corregir

--- src/app.py
+++ src/app.py
@@ -1,10 +1,9 @@
 import os
+import seaborn as sns
 import pandas as pd
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 # Cargar las variables del archivo .env
 load_dotenv()
@@ -13,18 +12,21 @@ load_dotenv()
 client_id = os.environ.get('CLIENT_ID')
 client_secret = os.environ.get('CLIENT_SECRET')
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id,
+con = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id,
                                                            client_secret=client_secret))
+artist_id = '4gzpq5DPGxSnKTe4SA8HAU'
 
-artist_id = "4gzpq5DPGxSnKTe4SA8HAU"
-
-response = sp.artist_top_tracks(artist_id)
+response = con.artist_top_tracks(artist_id)
 if response:
-    tracks = response["tracks"]
-    tracks = [{k: (v / (1000 * 60)) % 60 if k == "duration_ms" else v for k, v in track.items()
-               if k in ["name", "popularity", "duration_ms"]} for track in tracks]
-    
-tracks_df = pd.DataFrame.from_records(tracks)
-tracks_df.sort_values(["popularity"], inplace=True)
+  tracks = response["tracks"]
+  tracks = [{k: (v/(1000*60))%60 if k == "duration_ms" else v for k, v in track.items() if k in ["name", "popularity", "duration_ms"]} for track in tracks]
 
-print(tracks_df.tail())
+tracks_df = pd.DataFrame.from_records(tracks)
+tracks_df.sort_values(["popularity"], inplace = True, ascending=False)
+
+print(tracks_df.head(3))
+
+#VISUALIZACION
+scatter_plot = sns.scatterplot(data = tracks_df, x = "popularity", y = "duration_ms")
+fig = scatter_plot.get_figure()
+fig.savefig("scatter_plot.png")
